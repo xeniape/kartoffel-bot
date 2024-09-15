087f4c1ca551e3707193229806bd80aa2213248c
xeniape <xeniafischer@hotmail.de>
2024-09-15 14:46:13 +0200

    try out bot implementation

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,83 @@
 
 use kartoffel::*;
 
+enum Direction {
+    FRONT,
+    LEFT,
+    RIGHT,
+}
+
 #[no_mangle]
 fn main() {
-    // NOTE If you get lost during the tutorial, see ../SPOILERS.md
-
     loop {
-        motor_wait();
-        motor_step();
+        radar_wait();
+        let scan = radar_scan_5x5();
 
-        motor_wait();
-        motor_step();
+        if let Some(enemy_position) = find_enemy(scan) {
+            move_based_on_direction(convert_position_to_direction(enemy_position));
+            if enemy_position == (1, 2) {
+                arm_wait();
+                arm_stab();
+            }
+        } else {
+            if scan[1][2] == '.' {
+                // Path is in front
+                motor_wait();
+                motor_step();
+            } else if scan[2][1] == '.' {
+                // Path is on left side
+                motor_wait();
+                motor_turn_left();
+            } else if scan[2][3] == '.' {
+                // Path is on right side
+                motor_wait();
+                motor_turn_right();
+            }
+            serial_send('_')
+        }
+    }
+}
 
-        motor_wait();
-        motor_step();
+fn find_enemy(map: [[char; 5]; 5]) -> Option<(usize, usize)> {
+    let mut nearest_enemy: Option<(usize, usize)> = None;
+    let mut best_distance: usize = 5;
 
-        motor_wait();
-        motor_turn_right();
+    for i in 0..5 {
+        for j in 0..5 {
+            if i == 2 && j == 2 {
+                continue;
+            }
+            if map[i][j] == '@' {
+                let enemy_distance = get_distance_to_player((i, j));
+                if enemy_distance < best_distance {
+                    nearest_enemy = Some((i, j));
+                    best_distance = enemy_distance;
+                }
+            }
+        }
+    }
+    nearest_enemy
+}
+
+fn get_distance_to_player((i, j): (usize, usize)) -> usize {
+    i.abs_diff(2) + j.abs_diff(2)
+}
+
+fn convert_position_to_direction((i, j): (usize, usize)) -> Direction {
+    match (i, j) {
+        (0, _) | (1, _) => Direction::FRONT,
+        (_, 0) | (_, 1) => Direction::LEFT,
+        (_, _) => Direction::RIGHT,
+    }
+}
+
+fn move_based_on_direction(direction: Direction) {
+    motor_wait();
+    match direction {
+        Direction::FRONT => {
+            motor_step();
+        }
+        Direction::RIGHT => motor_turn_right(),
+        Direction::LEFT => motor_turn_left(),
     }
 }
